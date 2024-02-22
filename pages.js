2af4fc397afa87b2f4749a3a8e05071ad6387275
fenick76 <160507866+fenick76@users.noreply.github.com>
2024-02-22 14:55:59 -0500

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,4 @@
+export {default as Login} from './src/pages/Login'
+export {default as Homepage} from './src/pages/Homepage'
+export {default as Starting} from './src/pages/Starting'
+export {default as Gallery} from './src/pages/Gallery'
