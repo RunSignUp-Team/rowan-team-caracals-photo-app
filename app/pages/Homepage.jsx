4d6d0fb60625e33ec8c39667bae1bc7136fcff94
andrea stem <steman57@students.rowan.edu>
2024-02-27 14:06:26 -0500

    Start of homepg fix

--- app/pages/Homepage.jsx
+++ app/pages/Homepage.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 //import Ionicons from '@expo/vector-icons/Ionicons';
 import { FontAwesome } from '@expo/vector-icons';
 import { router } from "expo-router";
 import GalleryPageButton from "../../components/buttons/GalleryButton";
+import { StatusBar } from 'expo-status-bar';
+import {Image, StyleSheet, Text, TouchableHighlight, SafeAreaView, View, TouchableOpacity} from 'react-native';
+//import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+
+const logoImage = require('../../assets/brand/logo-circle.png')
+const logoText = require('../../assets/brand/logo-text.png')
 
 const Homepage = () => {
     return (
-        <View style={styles.container}>
-            <TouchableHighlight style = {styles.button}>
-                <FontAwesome name="cog" size={30} color="black" />
-            </TouchableHighlight>
-            <StatusBar style="auto" />
-            <GalleryPageButton></GalleryPageButton>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity style = {styles.settingsButton}>
+                <FontAwesome name="cog" size={33} color="black" />
+            </TouchableOpacity>
+            <Image
+                style={styles.circleLogo}
+                source={logoImage}/>
+            <Image
+                style={styles.textLogo}
+                source={logoText}/>
+            <Text style={styles.welcomeText}>Welcome, Bruce!</Text>
+            <View style={styles.line} />
+
+            <View style={styles.bottomLayer}>
+                <GalleryPageButton></GalleryPageButton>
+
+            </View>
+
+           
+        </SafeAreaView>
+
+
+
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -25,17 +48,70 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    button: {
+    bottomLayer: {
+       // flex: 1/8,
         position: 'absolute',
-        top: 50,
-        right: 15,
-        padding: 10,
-        borderRadius: 5,
+        bottom: 0, // Positioned at the bottom of the container
+        height: '10%', // 1/8th of the screen height
+        backgroundColor: '#EF509C',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    settingsButton: {
+        position: 'absolute',
+        top: 54,
+        right: 20,
+       padding: 2,
+        borderRadius: 1,
 
     },
+    circleLogo: {
+        width: 190,
+        height: 190,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '-22%'
+    },
+    textLogo: {
+        width: 160,
+        height: 160,
+        resizeMode: 'contain',
+        position: "absolute",
+        top: -8,
+        left: 15
+
+    },
+    welcomeText: {
+        alignItems: 'center',
+        fontSize: 35,
+        top: -170
+    },
+    galleryButton: {
+        position: 'absolute',
+        bottom: 20,
+        left: 90,
+    },
+    statusButton: {
+        position: 'absolute',
+        top: 57,
+        right: 70,
+        padding: 2,
+        borderRadius: 1
+
+
+    },
+    line: {
+        position: 'absolute',
+        top: 111,
+        width: '100%',
+        height: 1.2,
+        backgroundColor: 'black',
+    }
 
 
 });
 
 
-export default Homepage
+
+export default Homepage;
