b1ad2503179ed670ef912b5cf5b49929ee24cb34
ahmedkcd <kaceda24@students.rowan.edu>
2024-02-28 18:02:49 -0500

    Update Login.jsx

--- app/pages/Login.jsx
+++ app/pages/Login.jsx
@@ -5,14 +5,21 @@ import BasicButton from "../../components/buttons/BasicButton";
 import { router } from "expo-router";
 
 const Login = () => {
+
+    const handleSubmit = () => {
+        console.log('Name:', Username);
+        console.log('Password:', Password);
+      };
+
     const goToHomePage = () => {
         router.navigate('pages/Homepage');
       };
+
     return (
         <SafeAreaView style={styles.container}>
-            <BasicTextInput title='Username/Email'></BasicTextInput>
-            <BasicTextInput title='Password'></BasicTextInput>
-            <BasicButton title='Submit' fontFamily={'SF-Pro-Text-Bold'} onPress={goToHomePage}></BasicButton>
+            <BasicTextInput name='Username' title='Username/Email'></BasicTextInput>
+            <BasicTextInput name='Password' title='Password'></BasicTextInput>
+            <BasicButton title='Submit' fontFamily={'SF-Pro-Text-Bold'} onPress={handleSubmit}></BasicButton>
         </SafeAreaView>
     );
 }
