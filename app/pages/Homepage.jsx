9ffa202d68dde13d93f4a75a63e30e4045abdcbe
MVPHitter1 <daetgen5@gmail.com>
2024-03-26 18:02:18 -0400

    Date and Album Visibility is fully working and styled

--- app/pages/Homepage.jsx
+++ app/pages/Homepage.jsx
@@ -159,13 +159,34 @@ return (
                 source={logoText} />
 
             <Text style={styles.welcomeText}>Welcome, {firstName}!</Text>
-            <View style={styles.line} />
-            <BasicDropdown 
-                title='Select race' data={raceNames}
-                onSelect={() => {
+            
+            <SelectDropdown 
+            data={raceNames}
+            defaultButtonText={'Select race'}
+            buttonStyle={styles.dropdown}
+            buttonTextStyle={styles.text}
+            dropdownIconPosition='right'
+            renderDropdownIcon={isOpened => {
+                return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={COLORS.black} size={16} />;
+              }}
+            search
+            searchPlaceHolder='Search...'
+            searchPlaceholderStyle={styles.text}
+            rowStyle={styles.contentContainer}
+            rowTextStyle={styles.text}
+            selectedRowStyle={styles.selectedRowStyle}
+            selectedRowTextStyle={styles.selectedRowTextStyle}
+            showsVerticalScrollIndicator={true}
+            disableAutoScroll={true}
+            listMode='SCROLLVIEW'
+
+                onSelect={(selectedItem, index) => {
+                    console.log(selectedItem, index)
                     setDateVisible(true)
                 }}
-            />
+            >
+            </SelectDropdown>
+        
 
             {
                     dateVisible ? <SelectDropdown
@@ -301,9 +322,33 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 1.2,
         backgroundColor: 'black',
+    },
+
+    dropdown: {
+        borderRadius: 15,
+        width: '80%',
+        height: 55,
+        borderColor: 'black',
+        borderWidth: 1,
+        backgroundColor: COLORS.off_white,
+    },
+    text:{
+        textAlign: 'left',
+        fontFamily: 'SF-Pro-Display-Regular',
+        paddingBottom: 5,
+    },
+    contentContainer: {
+        backgroundColor: COLORS.off_white,
+        borderBlockColor: COLORS.light_gray,
+        paddingBottom: 15,
+    },
+    selectedRowStyle: {
+        backgroundColor: COLORS.RSU_pink,
+    },
+    selectedRowTextStyle: {
+        color: COLORS.off_white,
     }
-
-
+    
 });
 
 
